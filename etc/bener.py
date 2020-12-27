77a810130d61ac01171a72a77418d3e81bbbdeab
Avindeso <74377866+Fuziro@users.noreply.github.com>
2020-12-27 20:08:07 +0700

    Add files via upload

---
+++ etc/bener.py
@@ -0,0 +1,19 @@
+from warn.warn import *
+# -*- coding: utf-8 -*-
+import os,sys,time
+
+def wr(z):
+	for x in z + '\n':
+		sys.stdout.write(x);sys.stdout.flush();time.sleep(0./1000)
+def bnr():
+ wr(f"""{b}╔═╗┌┐┌┌─┐┌┐┌┌─┐┬ ┬┌┐   {b}╔╗ ┬─┐┬ ┬┌┬┐┌─┐┬  
+{p}╠═╣││││ │││││  └┬┘├┴┐  ╠╩╗├┬┘│ │ │ ├─┤│   
+╩ ╩┘└┘└─┘┘└┘└─┘ ┴ └─┘  ╚═╝┴└─└─┘ ┴ ┴ ┴┴─┘ 
+{k}════════════════════════════════════════════
+{p}[{h}+{p}]{h}Team   {p}: {b}Avindeso
+{p}[{h}+{p}]{h}Github {p}: {b}https://github.com/Fuziro
+{p}[{h}+{p}]{h}Script {p}: {b}Brutal SMS
+{k}════════════════════════════════════════════
+""")
+
+
